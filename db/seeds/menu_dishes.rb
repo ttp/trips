1a2fc10090af9934fd37cd911bd664e76bc0e377
Taras Taday <human.ttp@gmail.com>
2013-09-21 10:18:21 +0300

    added valid dishes

--- db/seeds/menu_dishes.rb
+++ db/seeds/menu_dishes.rb
@@ -10,7 +10,6 @@ Menu::DishProduct.delete_all
 category = dish = nil
 locales = [:ru,:ua]
 filename = File.expand_path('../menu_dishes.csv', __FILE__)
-products = [Menu::Product.first, Menu::Product.last]
 CSV.foreach(filename, :headers => true) do |row|
   if row.field('type') == '1'
     category = Menu::DishCategory.new
@@ -20,7 +19,7 @@ CSV.foreach(filename, :headers => true) do |row|
       end
     end
     category.save
-  else
+  elsif row.field('type') == '2'
     dish = category.dishes.create
     locales.each do |locale|
       Globalize.with_locale(locale) do
@@ -28,13 +27,20 @@ CSV.foreach(filename, :headers => true) do |row|
       end
     end
     dish.save
-
-    products.each do |product|
+  else
+    product = nil
+    locales.each do |locale|
+      if row.field(locale.to_s) != ""
+        products = Menu::Product.with_translations(locale).where("name = ?", row.field(locale.to_s))
+        product = products[0] if products.size > 0
+        break
+      end
+    end
+    unless product.nil?
       dish_product = dish.dish_products.create
       dish_product.product = product
-      dish_product.weight = [10, 20, 30, 40].sample
+      dish_product.weight = row.field('weight').to_i
       dish_product.save
     end
-
   end
 end
