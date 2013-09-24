ec898b0d467228ac405c9fb2447226f6507e83bc
Taras Taday <human.ttp@gmail.com>
2013-09-24 11:19:25 +0300

    added minifier config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -13,6 +13,8 @@ Pohody::Application.configure do
 
   # Compress JavaScripts and CSS
   config.assets.compress = true
+  config.assets.js_compressor = :uglifier
+  config.assets.css_compressor = :scss
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = false
