e23434a4b4f4ea7457a6f6f8c1d21f059f9ab44f
Taras Taday <human.ttp@gmail.com>
2014-02-02 15:07:28 +0200

    always show switch user select

--- config/initializers/switch_user.rb
+++ config/initializers/switch_user.rb
@@ -33,9 +33,9 @@ SwitchUser.setup do |config|
   # if it returns true, the switch user select box will be shown,
   # else the select box will not be shown
   # if you switch from admin to "user", the current_user param is "user"
-  #config.view_guard = lambda { |current_user, request, original_user|
-  #  (current_user && current_user.admin?) || (original_user && original_user.admin?)
-  #}
+  config.view_guard = lambda { |current_user, request, original_user|
+    true
+  }
 
   # redirect_path is a block, it returns which page will be redirected
   # after switching a user.
