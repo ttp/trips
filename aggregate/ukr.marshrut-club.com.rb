1780db0142c54b95b46e2f62aa269c0ae64a1caf
Taras Taday <human.ttp@gmail.com>
2014-03-10 12:47:50 +0200

    updated trips for 2014

--- aggregate/ukr.marshrut-club.com.rb
+++ aggregate/ukr.marshrut-club.com.rb
@@ -24,7 +24,7 @@ $month_map = {
 }
 
 def get_dates(dates_str)
-  year = 2013
+  year = 2014
   if /\d–\d/ =~ dates_str
     nums, month = dates_str.split(' ')
     nums = nums.split("–")
@@ -48,6 +48,7 @@ def get_region_name(region)
     when /непал/i then 'Nepal'
     when /кавказ/i then 'Caucasus'
     when /румун/i then 'Romania'
+    when /туреч/i then 'Turkey'
     else 'Other'
   end
 end
@@ -64,38 +65,31 @@ def get_track(url)
 end
 
 $domain = 'ukr.marshrut-club.com'
-page_path = '/'
+calendar_path = '/calendar'
 http = Net::HTTP.new($domain)
-resp = http.get(page_path)
+resp = http.get(calendar_path)
 doc = Nokogiri::HTML(resp.body)
 
-title = "Розклад походів"
-title_node = doc.css("div.column h2").select {|node| node.inner_text == title}.first
-
-
-list_node = title_node.next_element
+table_rows = doc.css("table.t_alldate tr")
 
 puts ["name", "start_date", "end_date", "region", "track", "url", "has_guide", "available_places"].to_csv
-while true do
-  if list_node['class'] == 'main_cal_title_r'
-    region = list_node.at_css("a").inner_text
-  elsif list_node['class'] == 'main_cal_elem_r'
-    dates_text = list_node.inner_text.strip
-    link_node = list_node.at_css('a')
-    start_date, end_date = get_dates(dates_text)
-    url = "http://" + $domain + link_node.attr("href")
-    puts [
-      link_node.inner_text.strip,
-      start_date,
-      end_date,
-      get_region_name(region),
-      get_track(url),
-      url,
-      "yes",
-      10
-    ].to_csv
-  else
-    break
-  end
-  list_node = list_node.next_element
+table_rows.each_with_index do |row, i|
+  cells = row.css("td")
+  next if cells.size < 5
+  region = cells[1].inner_text.strip
+
+  dates_text = cells[0].inner_text.strip
+  link_node = cells[2].at_css('a')
+  start_date, end_date = get_dates(dates_text)
+  url = "http://" + $domain + link_node.attr("href")
+  puts [
+    link_node.inner_text.strip,
+    start_date,
+    end_date,
+    get_region_name(region),
+    get_track(url),
+    url,
+    "yes",
+    10
+  ].to_csv
 end
