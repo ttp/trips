e07232b012e3585ecb0e3d5e5fdb3d86de80d305
Taras Taday <human.ttp@gmail.com>
2015-02-12 21:49:16 +0200

    migrated calendar specs to rspec

--- spec/factories/trips.rb
+++ spec/factories/trips.rb
@@ -10,6 +10,18 @@ FactoryGirl.define do
 
     track
     user
+
+    factory :trip_in_region do
+      transient do
+        region_name 'Region'
+      end
+
+      before(:create) do |trip, evaluator|
+        region = Region.find_by_name(evaluator.region_name)
+        region = create(:region, name: evaluator.region_name) unless region
+        trip.track = create :track, region: region
+      end
+    end
   end
 
   factory :trip_user do
---
+++ spec/features/calendar/calendar_filters_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+feature 'Calendar filters', js: true do
+
+  background do
+    create_list :trip_in_region, 3, region_name: 'Carpathian'
+    create_list :trip_in_region, 2, region_name: 'Crimea'
+  end
+
+  scenario 'show list of regions' do
+    visit calendar_path
+    expect(page).to have_css("#filters", text: 'Carpathian')
+    expect(page).to have_css("#filters", text: 'Crimea')
+  end
+
+  scenario 'filter trips in calendar' do
+    visit calendar_path
+
+    find("#filters input[value='Carpathian']").click
+
+    expect(number_of_events).to eq 3
+  end
+
+  private
+
+  def number_of_events
+    events_cnt = 0
+    all("#calendar .events-count").each do |cnt_el|
+      events_cnt += cnt_el.text.to_i
+    end
+    events_cnt
+  end
+end
