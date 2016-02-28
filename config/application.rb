8f1707d35c01dbd6593295bf969b99d5c4196a72
Taras Taday <human.ttp@gmail.com>
2016-02-28 15:00:06 +0200

    disable en locale for now

--- config/application.rb
+++ config/application.rb
@@ -47,7 +47,7 @@ module Pohody
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    config.i18n.available_locales = [:uk, :ru, :en]
+    config.i18n.available_locales = [:uk, :ru]
     config.i18n.default_locale = :uk
 
     # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
