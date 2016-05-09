5422f55328e275320d7ba962e910c4aef0b2cc10
Taras Taday <human.ttp@gmail.com>
2016-05-09 21:59:10 +0300

    missed print precompile

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -10,4 +10,4 @@ Rails.application.config.assets.version = '1.2'
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
 
-Rails.application.config.assets.precompile += %w( init/* tracks.js trips.js )
+Rails.application.config.assets.precompile += %w( init/* tracks.js trips.js print/*.css)
