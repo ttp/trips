d7d8ca0021568749492862acee3662576bbd78c6
Taras Taday <human.ttp@gmail.com>
2016-05-09 21:54:03 +0300

    fixed assets precompile

--- config/application.rb
+++ config/application.rb
@@ -72,7 +72,6 @@ module Pohody
     config.assets.enabled = true
 
     # Version of your assets, change this if you want to expire all your assets
-    config.assets.version = '1.1'
     config.assets.initialize_on_precompile = true
 
     config.site = {
